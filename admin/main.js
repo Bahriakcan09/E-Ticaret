d8a6186ca516360d02e2c57d277a6fd8543522c6
Emre Aşcı <106581002+emrsc1@users.noreply.github.com>
2023-07-31 14:16:14 +0300

    geçici tasarımlar

---
+++ admin/main.js
@@ -0,0 +1,26 @@
+const listItems = document.querySelectorAll("#left-bar .list-group-item");
+
+listItems.forEach(item => {
+  item.addEventListener("click", function(e) {
+    listItems.forEach(item => item.classList.remove("active"));
+    this.classList.add("active");
+  });
+});
+
+
+const addBtn=document.getElementById("add");
+let showElement=(item)=>item.setAttribute("style","display:flex !important");
+let hideElement=(item)=>item.setAttribute("style","display:none !important");
+const cancelBtn=document.getElementById("cancel");
+const productAdd=document.querySelector("#productAdd");
+
+
+cancelBtn.addEventListener("click",(e)=>{
+    hideElement(productAdd);
+    e.preventDefault();
+})
+addBtn.addEventListener("click",(e)=>{
+    
+    showElement(productAdd);
+    e.preventDefault();
+})
